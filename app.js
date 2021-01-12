bf065510c51bda1ef0ca34a6fca0f02902f304b5
Dylan White <dylan.whitej@gmail.com>
2021-01-12 13:33:46 -0800

    DOM elements grabbed and validated

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // import functions and grab DOM elements
+const guessesRemaining = document.getElementById('guesses-remaining');
+const answerInput = document.getElementById('answer-input');
+const submitButton = document.getElementById('submit-button');
+const resultBox = document.getElementById('result-box');
+
+console.log(guessesRemaining);
+console.log(answerInput);
+console.log(submitButton);
+console.log(resultBox);
+
 
 // initialize state
 
