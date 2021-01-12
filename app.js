d304be69205e4df6aecfc221c9877dc35b7a65fc
Dylan White <dylan.whitej@gmail.com>
2021-01-12 14:02:56 -0800

    adds compareNumbers and testing

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './utils';
+
 const guessesRemaining = document.getElementById('guesses-remaining');
 const answerInput = document.getElementById('answer-input');
 const submitButton = document.getElementById('submit-button');
 const resultBox = document.getElementById('result-box');
 
-console.log(guessesRemaining);
-console.log(answerInput);
-console.log(submitButton);
-console.log(resultBox);
+// console.log(guessesRemaining);
+// console.log(answerInput);
+// console.log(submitButton);
+// console.log(resultBox);
 
 
 // initialize state
+let guessesCounter = 4;
+let correctNumber = Math.ceil(Math.random() * 20);
+// console.log(correctNumber);
+
 
 // set event listeners to update state and DOM
+submitButton.addEventListener('click', {
+
+
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,49 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { compareNumbers } from '../utils.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('when given a correct answer of 10 and a guess of 2, function should return -1', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = -1;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = compareNumbers(2, 10);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('when given a correct answer of 10 and a guess of 11, function should return 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(11, 10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('when given a correct answer of 10 and a guess of 10, function should return 0', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 0;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(10, 10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
